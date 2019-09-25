9bb0871f2118bff549e10da846a34d34a9088086
yghimire1994 <yghimire1994@gmail.com>
2019-09-25 12:17:29 -0500

    follower page

--- src/main/java/edu/mum/cs/controller/follower/FollowerController.java
+++ src/main/java/edu/mum/cs/controller/follower/FollowerController.java
@@ -1,6 +1,19 @@
 package edu.mum.cs.controller.follower;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet("/followers")
 public class FollowerController extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("views/followers.jsp");
+        rd.forward(req,resp);
+
+
+    }
 }
